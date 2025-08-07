df5144b9630af5e40d680b7fd331a5623d87c407
MohamedThoufiq27 <mohamedthoufiq981@gmail.com>
2025-08-07 22:42:12 +0530

    added front-end

---
+++ front-end/src/components/Search.jsx
@@ -0,0 +1,25 @@
+
+
+const Search = ({searchTerm,setSearchTerm,setSubmitText}) => {
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Search :',searchTerm);
+    setSubmitText(searchTerm);
+    setSearchTerm('');
+  }
+  return (
+    <div className="w-full border-1 rounded-3xl border-zinc-400 p-4 py-6">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="w-full h-full border-none focus:outline-none"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search URL to detect"
+        />
+      </form>
+    </div>
+  )
+}
+
+export default Search
